461d9523c47520b0b0c6922a9b4d92404e497ee5
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-01-31 17:21:24 +0900

    🎉add : QuestPlayerController 플레이어 컨트롤러 생성

---
+++ Quest7/Source/Quest7/Public/QuestPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "QuestPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUEST7_API AQuestPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
