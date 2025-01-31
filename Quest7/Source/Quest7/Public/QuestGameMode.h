ec67771ae74aeaafb9371d22f9ee26371b964a3f
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-01-31 17:20:27 +0900

    🎉add : QuestGameMode 게임 모드 생성 및 플레이어 컨트롤러, 폰 설정

---
+++ Quest7/Source/Quest7/Public/QuestGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "QuestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUEST7_API AQuestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AQuestGameMode();
+	
+};
